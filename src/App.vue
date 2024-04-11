af3375a6d90ff89360d09c11a5db1c25f8c04b4e
sofiacottone <cottonesofia@outlook.it>
2024-04-11 17:19:21 +0200

    feat: import main cards section component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppMainCards from './components/AppMainCards.vue';
+
 export default {
     components: {
         AppHeader,
+        AppMainCards,
     }
 }
 </script>
 
 <template>
     <AppHeader></AppHeader>
+
+    <main>
+        <AppMainCards></AppMainCards>
+    </main>
 </template>
 
 <style lang="scss">
