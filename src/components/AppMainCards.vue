32ee83f4a5a915455ec08e9c28619fe9d35dcad7
sofiacottone <cottonesofia@outlook.it>
2024-04-11 17:18:27 +0200

    feat: add main cards section component

---
+++ src/components/AppMainCards.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    components: {
+        name: 'AppMainCards'
+    }
+}
+</script>
+
+<template>
+    <section class="bg-dark">
+        <div class="container h-100">
+            <div class="d-flex justify-content-center align-items-center text-white h-100">Insert content here</div>
+        </div>
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    height: 130px;
+}
+</style>
