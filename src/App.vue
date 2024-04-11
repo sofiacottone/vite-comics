7fb3d55033effa7cd382afcfe9afe4081d87950a
sofiacottone <cottonesofia@outlook.it>
2024-04-11 17:05:13 +0200

    feat: import header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
 export default {
     components: {
-
+        AppHeader,
     }
 }
 </script>
 
 <template>
-
+    <AppHeader></AppHeader>
 </template>
 
 <style lang="scss">
