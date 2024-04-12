270f002a49d130d198afd2f411febdabb7659482
sofiacottone <cottonesofia@outlook.it>
2024-04-12 15:52:11 +0200

    fix: minor responsiveness changes

--- src/components/AppMainPages.vue
+++ src/components/AppMainPages.vue
@@ -10,30 +10,36 @@ export default {
     <section class="">
         <div class="container h-100">
             <div class="d-flex justify-content-center align-items-center text-white h-100">
-                <ul class="d-flex gap-4 mb-0 text-uppercase">
-                    <li>
-                        <span class="ms-img-container">
+                <ul class="d-flex gap-4 mb-0 ps-0 text-uppercase">
+                    <li class="d-flex align-items-center">
+                        <div class="ms-img-container">
                             <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        </span>
-                        <span class="ms-2">testo</span>
+                        </div>
+                        <div class="ms-2">digital comics</div>
                     </li>
-                    <li>
-                        <span class="ms-img-container">
-                            <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        </span>
-                        <span class="ms-2">testo</span>
+                    <li class="d-flex align-items-center">
+                        <div class="ms-img-container">
+                            <img class="img-fluid" src="../assets/img/buy-comics-merchandise.png" alt="">
+                        </div>
+                        <div class="ms-2">dc merchandise</div>
                     </li>
-                    <li>
-                        <span class="ms-img-container">
-                            <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        </span>
-                        <span class="ms-2">testo</span>
+                    <li class="d-flex align-items-center">
+                        <div class="ms-img-container">
+                            <img class="img-fluid" src="../assets/img/buy-comics-subscriptions.png" alt="">
+                        </div>
+                        <div class="ms-2">subscription</div>
                     </li>
-                    <li>
-                        <span class="ms-img-container">
-                            <img class="img-fluid" src="../assets/img/buy-comics-digital-comics.png" alt="">
-                        </span>
-                        <span class="ms-2">testo</span>
+                    <li class="d-flex align-items-center">
+                        <div class="ms-img-container">
+                            <img class="img-fluid" src="../assets/img/buy-comics-shop-locator.png" alt="">
+                        </div>
+                        <div class="ms-2">comic shop locator</div>
+                    </li>
+                    <li class="d-flex align-items-center">
+                        <div class="ms-img-container">
+                            <img class="img-fluid" src="../assets/img/buy-dc-power-visa.svg" alt="">
+                        </div>
+                        <div class="ms-2">dc power visa</div>
                     </li>
                 </ul>
             </div>
@@ -53,7 +59,25 @@ ul {
 
     .ms-img-container {
         display: inline-block;
-        width: 50px;
+        width: 60px;
+
+        img {
+            object-fit: contain;
+            aspect-ratio: 3/2;
+        }
+    }
+}
+
+@media screen and (max-width: 1199px) {
+    ul {
+        li {
+            flex-direction: column;
+            text-align: center;
+
+            .ms-img-container {
+                padding-bottom: 10px;
+            }
+        }
     }
 }
 </style>
